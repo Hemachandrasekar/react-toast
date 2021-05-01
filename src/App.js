64f5f216f325201e8151308e4fbb12bd1cb04d74
Hema Victor <m.hema10@gmail.com>
2021-05-01 11:54:58 +0100

    Add new component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import {Toast} from "./component/Toast"
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [showToast,setShowToast]=useState(false);
+  const handleToast =()=>{
+    setShowToast(true)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello </h1>
+    <button onClick={handleToast}>Show Toast</button>    
+    { showToast && <Toast />}
+    </>
   );
 }
 
---
+++ src/component/Toast/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+const Toast =()=>{
+ return(
+     <div className="toast-container">
+        <span className="toast-child">Hello</span> 
+     </div>
+ )
+}
+export {Toast}
